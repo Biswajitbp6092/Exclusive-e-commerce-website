467641ac3829f2f764155d38503589b502f41fe2
biswajitecd <biswajit@ethicalden.com>
2025-09-04 17:10:37 +0530

    done

--- src/app/_ component/Home-page/HomePage.jsx
+++ src/app/_ component/Home-page/HomePage.jsx
@@ -1,26 +1,26 @@
-import React from 'react'
-import HeroSection from './Hero-section/HeroSection'
-import FlashSales from './Flash-Sales/FlashSales'
-import BrowseByCategory from './Browse-By-Category/BrowseByCategory'
-import BestSellingProducts from './Best-Selling-Products/BestSellingProducts'
-import Categories from './Categories/Categories'
-import ExploreOurProducts from './Explore-Our-Products/ExploreOurProducts'
-import NewArrival from './New-Arrival/NewArrival'
-import CustomerSupport from '../Utilities/CustomerSupport/CustomerSupport'
+import React from "react";
+import HeroSection from "./Hero-section/HeroSection";
+import FlashSales from "./Flash-Sales/FlashSales";
+import BrowseByCategory from "./Browse-By-Category/BrowseByCategory";
+import BestSellingProducts from "./Best-Selling-Products/BestSellingProducts";
+import Categories from "./Categories/Categories";
+import ExploreOurProducts from "./Explore-Our-Products/ExploreOurProducts";
+import NewArrival from "./New-Arrival/NewArrival";
+import CustomerSupport from "../Utilities/CustomerSupport/CustomerSupport";
 
 const HomePage = () => {
   return (
     <div>
-        <HeroSection/>
-        <FlashSales/>
-        <BrowseByCategory/>
-        <BestSellingProducts/>
-        <Categories/>
-        <ExploreOurProducts/>
-        <NewArrival/>
-        <CustomerSupport/>
+      <HeroSection />
+      <FlashSales />
+      <BrowseByCategory />
+      <BestSellingProducts />
+      <Categories />
+      <ExploreOurProducts />
+      <NewArrival />
+      <CustomerSupport />
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
